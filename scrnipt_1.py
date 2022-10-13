1a2e701125cec14146e8baa4558eb800236a2048
MagGoldi <StivenH0king@yandex.ru>
2022-10-13 11:21:02 +0400

    creat file + working with code

--- scrnipt_1.py
+++ scrnipt_1.py
@@ -2,18 +2,18 @@ import os
 import datetime
 
 
-def work_file(path_to_csv=os.path.join("PYTHON", "dataset.csv")):
+def work_file(path_to_csv=os.path.join("File_folder", "dataset.csv")):
     file_main = open(path_to_csv, "a", encoding="utf-8")
-    today = datetime.date.today()
+    today = str(datetime.date.today())
     file_main.write(today)
     file_main.write("\n")
 
 def make_dir():
-    if not os.path.isdir("PYTHON"):
-        os.mkdir("PYTHON")
+    if not os.path.isdir("File_folder"):
+        os.mkdir("File_folder")
 
-def run_1(path_to_csv=os.path.join("PYTHON", "dataset.csv")):
-    file_main = open(path_to_csv, "w", encoding="utf-8")
+def run_1(path_to_csv=os.path.join("File_folder", "dataset.csv")):
     make_dir()
+    file_main = open(path_to_csv, "w", encoding="utf-8")
     work_file()
     file_main.close()
