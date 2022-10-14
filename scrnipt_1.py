8734dbae5f5e3df38f27115cc1caa81d2e03b53b
MagGoldi <StivenH0king@yandex.ru>
2022-10-14 19:50:38 +0400

    file

--- scrnipt_1.py
+++ scrnipt_1.py
@@ -6,8 +6,9 @@ def work_file(path_to_csv=os.path.join("File_folder", "dataset.csv")):
     file_main = open(path_to_csv, "a", encoding="utf-8")
     year, mounth, day = 2022, 10, 14
     for day in range(day, 32):
-        date_f = str(datetime.date(year, mounth, day))
-        file_main.write(date_f)
+        date_f = str(datetime.date(year, mounth, day)) #2022-
+        data_f = "data" + str(day-13)
+        file_main.write(date_f + ";" + data_f)
         file_main.write("\n")
 
 def make_dir():
