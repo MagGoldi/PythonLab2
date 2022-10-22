2376e03a4f1151f22d0d58fdea3c1dbbc4533c08
MagGoldi <StivenH0king@yandex.ru>
2022-10-22 11:21:56 +0400

    work

--- scrnipt_1.py
+++ scrnipt_1.py
@@ -1,25 +1,25 @@
 import os
 import datetime
+import csv
 
+#Написать скрипт, который разобъёт исходный csv файл на файл X.csv и Y.csv, 
+#с одинаковым количеством строк. Первый будет содержать даты, второй - данные.
 
-def work_file(path_to_csv=os.path.join("File_folder", "dataset.csv")):
-    file_main = open(path_to_csv, "a", encoding="utf-8")
-    year, mounth, day = 2022, 10, 14
-    for day in range(day, 32):
-        date_f = str(datetime.date(year, mounth, day)) #2022-
-        data_f = "data" + str(day-13)
-        file_main.write(date_f + "," + data_f)
-        file_main.write("\n")
 
 def make_dir():
     if not os.path.isdir("File_folder"):
         os.mkdir("File_folder")
+    
 
 def split_file(path_to_csv=os.path.join("File_folder", "dataset.csv")):
     pass
 
 def run_1(path_to_csv=os.path.join("File_folder", "dataset.csv")):
     make_dir()
+    file_x = open("X.csv", "w", encoding="utf-8")
+    file_y = open("Y.csv", "w", encoding="utf-8")
     file_main = open(path_to_csv, "w", encoding="utf-8")
-    work_file()
+
+    file_x.close()
+    file_y.close()
     file_main.close()
