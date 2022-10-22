7e62f15e25effc63c125314596baaf61016d8eb1
MagGoldi <StivenH0king@yandex.ru>
2022-10-22 14:08:33 +0400

    pip8

--- scrnipt_2.py
+++ scrnipt_2.py
@@ -3,14 +3,15 @@ import os
 import re
 
 
-#Написать скрипт, который разобъёт исходный csv файл на N файлов,
-# где каждый отдельный файл будет соответствовать одному году. 
-# Файлы называются по первой и последней дате, которую они содержат. 
+# Написать скрипт, который разобъёт исходный csv файл на N файлов,
+# где каждый отдельный файл будет соответствовать одному году.
+# Файлы называются по первой и последней дате, которую они содержат.
 # (если файл содержит данные с первого января 2001 по 31 декабря 2001, то файл назвать 20010101_20011231.csv)
 
 def make_dir():
     if not os.path.isdir("File_folder/scrnipt_2"):
         os.mkdir("File_folder/scrnipt_2")
+
 
 def work_file(date_1, date_2, list1_years):
     name_file = 'File_folder/scrnipt_2/' + date_1 + "_" + date_2 + ".csv"
@@ -19,7 +20,6 @@ def work_file(date_1, date_2, list1_years):
         writer = csv.writer(namefile)
         for i in range(len(list1_years)):
             writer.writerow(list1_years[i])
-        
 
 
 def run_2():
@@ -30,19 +30,18 @@ def run_2():
         file_reader = csv.reader(csvfile)
         for row in file_reader:
             set1.add(row[0][:4])
-    set1 = sorted(list(set1), reverse=True) 
-    n = len(set1)      
+    set1 = sorted(list(set1), reverse=True)
+    n = len(set1)
 
     with open('File_folder/dataset.csv', 'r', newline='') as csvfile:
-        file_reader = list(csv.reader(csvfile)) 
+        file_reader = list(csv.reader(csvfile))
         for i in range(n):
             for j in range(len(file_reader)):
-                if(file_reader[j][0][:4] == set1[i]): list1_years.append(file_reader[j])
-            print(list1_years[0][0])              
+                if (file_reader[j][0][:4] == set1[i]): list1_years.append(file_reader[j])
+            print(list1_years[0][0])
             date_1 = str(re.sub(r'[-]', '', list1_years[0][0]))
             date_2 = str(re.sub(r'[-]', '', list1_years[-1][0]))
             work_file(date_1, date_2, list1_years)
             list1_years = []
 
-    csvfile.close()        
-
+    csvfile.close()
