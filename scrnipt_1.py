1626af1bdd52d67a9c126b3232d008803d73793a
MagGoldi <StivenH0king@yandex.ru>
2022-10-22 11:48:03 +0400

    done

--- scrnipt_1.py
+++ scrnipt_1.py
@@ -1,25 +1,33 @@
-import os
-import datetime
 import csv
-
-#Написать скрипт, который разобъёт исходный csv файл на файл X.csv и Y.csv, 
-#с одинаковым количеством строк. Первый будет содержать даты, второй - данные.
+import os
 
 
 def make_dir():
     if not os.path.isdir("File_folder"):
         os.mkdir("File_folder")
-    
+    if not os.path.isdir("File_folder/scrnipt_1"):
+        os.mkdir("File_folder/scrnipt_1")   
 
-def split_file(path_to_csv=os.path.join("File_folder", "dataset.csv")):
-    pass
-
-def run_1(path_to_csv=os.path.join("File_folder", "dataset.csv")):
+def run_1():
     make_dir()
-    file_x = open("X.csv", "w", encoding="utf-8")
-    file_y = open("Y.csv", "w", encoding="utf-8")
-    file_main = open(path_to_csv, "w", encoding="utf-8")
+    list1 = []
+    with open('File_folder/dataset.csv', 'r', newline='') as csvfile:
+        file_reader = csv.reader(csvfile)
+        for row in file_reader:
+            list1.append(row)
+        print(list1)
 
-    file_x.close()
-    file_y.close()
-    file_main.close()
+    for i in range(0, len(list1)):
+        with open("File_folder/scrnipt_1/X.csv", "a", newline="") as csvfile_x:
+            all_data = [list1[i][0]]
+            writer = csv.writer(csvfile_x)
+            writer.writerow(all_data)
+
+        with open("File_folder/scrnipt_1/Y.csv", "a", newline="") as csvfile_y:
+            all_data = list1[i][1:]
+            writer = csv.writer(csvfile_y)
+            writer.writerow(all_data)
+
+    csvfile_x.close()
+    csvfile_y.close()
+    csvfile.close()        
