1c795017c0614f02629c5f50bc112c6874d8bea3
MagGoldi <StivenH0king@yandex.ru>
2022-10-24 22:11:15 +0400

    appdate

--- scrnipt_1.py
+++ scrnipt_1.py
@@ -12,23 +12,21 @@ def make_dir():
 def run_1():
     make_dir()
     list1 = []
-    with open('File_folder/dataset.csv', 'r', newline='', encoding = "utf-8") as csvfile:
+    with open('File_folder/dataset.csv', 'r', newline='') as csvfile:
         file_reader = csv.reader(csvfile)
         for row in file_reader:
             list1.append(row)
 
-    for i in range(0, len(list1)):
-        with open("File_folder/scrnipt_1/X.csv", "a", newline="", encoding = "utf-8") as csvfile_x:
-            all_data = [list1[i][0]]
-            writer = csv.writer(csvfile_x)
-            writer.writerow(all_data)
+    with open("File_folder/scrnipt_1/X.csv", 'w', newline="") as csvfile_x:  
+        for i in range(0, len(list1)):
+                all_data = [list1[i][0]]
+                writer = csv.writer(csvfile_x)
+                writer.writerow(all_data)
 
-        with open("File_folder/scrnipt_1/Y.csv", "a", newline="", encoding = "utf-8") as csvfile_y:
+    with open("File_folder/scrnipt_1/Y.csv", "w", newline="") as csvfile_y:
+        for i in range(0, len(list1)):
             all_data = list1[i][1:]
             writer = csv.writer(csvfile_y)
             writer.writerow(all_data)
     
-    csvfile_x.close()
-    csvfile_y.close()
-    csvfile.close()
     print("\nscript_1 has finished working\n")
