9c55975e52e09e92caa2b084c5a3f8057767da7f
MagGoldi <StivenH0king@yandex.ru>
2022-10-27 21:24:00 +0400

    rework s_1

--- scrnipt_1.py
+++ scrnipt_1.py
@@ -9,21 +9,21 @@ def make_dir():
         os.mkdir("File_folder/scrnipt_1")
 
 
-def run_1(path_to_csv=os.path.join("PYTHON", "File_folder", "dataset.csv" )):
+def run_1(path_to_csv=os.path.join("C:/","PYTHON", "PythonLab2-1", "File_folder")):
     make_dir()
     list1 = []
-    with open(path_to_csv, 'r', newline='') as csvfile:
+    with open(path_to_csv + "dataset.csv", 'r', newline='') as csvfile: #C:\PYTHON\PythonLab2-1\File_folder\dataset.csv
         file_reader = csv.reader(csvfile)
         for row in file_reader:
             list1.append(row)
 
-    with open("File_folder/scrnipt_1/X.csv", 'w', newline="") as csvfile_x:  
+    with open(path_to_csv + "scrnipt_1/X.csv", 'w', newline="") as csvfile_x:  
         for i in range(0, len(list1)):
                 all_data = [list1[i][0]]
                 writer = csv.writer(csvfile_x)
                 writer.writerow(all_data)
 
-    with open("File_folder/scrnipt_1/Y.csv", "w", newline="") as csvfile_y:
+    with open(path_to_csv + "scrnipt_1/Y.csv", "w", newline="") as csvfile_y:
         for i in range(0, len(list1)):
             all_data = list1[i][1:]
             writer = csv.writer(csvfile_y)
