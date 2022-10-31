a6d95997be06057b4e4b23b17a0fba46c1adb554
MagGoldi <StivenH0king@yandex.ru>
2022-10-31 16:46:19 +0400

    rework

--- scrnipt_1.py
+++ scrnipt_1.py
@@ -1,32 +1,33 @@
-import csv
+import csv 
 import os
 
+def make_dir(path_fol: str, path_sc1: str) -> None: 
+    '''Принимает имя пути, если файла нет создает''' 
+    if not os.path.isdir(path_fol): os.mkdir(path_fol) 
+    if not os.path.isdir(path_sc1): os.mkdir(path_sc1)
 
-def make_dir():
-    if not os.path.isdir("File_folder"):
-        os.mkdir("File_folder")
-    if not os.path.isdir("File_folder/scrnipt_1"):
-        os.mkdir("File_folder/scrnipt_1")
-
-
-def run_1(path_to_csv=os.path.join("C:/","PYTHON", "PythonLab2-1", "File_folder")):
-    make_dir()
-    list1 = []
-    with open(path_to_csv + "\dataset.csv", 'r', newline='') as csvfile: #C:\PYTHON\PythonLab2-1\File_folder\dataset.csv
-        file_reader = csv.reader(csvfile)
-        for row in file_reader:
+def run_1(path_to_csv: str=os.path.join("C:/", "PYTHON", "PythonLab2-1", "File_folder")) -> None:
+    '''Основная функция работы скрипта''' 
+    path_fol, path_sc1 = "File_folder", "File_folder/scrnipt_1" 
+    make_dir(path_fol, path_sc1) 
+    list1 = [] 
+    with open(path_to_csv=os.path.join("C:/", "PYTHON", "PythonLab2-1", "File_folder", "dataset.csv", 'r', newline='')) as csvfile: 
+        file_reader = csv.reader(csvfile) 
+        for row in file_reader: 
             list1.append(row)
 
-    with open(path_to_csv + "/scrnipt_1/X.csv", 'w', newline="") as csvfile_x:  
+    with open(path_to_csv=os.path.join("C:/", "PYTHON", "PythonLab2-1", "File_folder", 
+    "scrnipt_1", "X.csv", 'w', newline="")) as csvfile_x:
         for i in range(0, len(list1)):
-                all_data = [list1[i][0]]
-                writer = csv.writer(csvfile_x)
-                writer.writerow(all_data)
+            all_data = [list1[i][0]]
+            writer = csv.writer(csvfile_x)
+            writer.writerow(all_data)
 
-    with open(path_to_csv + "/scrnipt_1/Y.csv", "w", newline="") as csvfile_y:
+    with open(path_to_csv=os.path.join("C:/", "PYTHON", "PythonLab2-1", "File_folder",
+    "scrnipt_1", "Y.csv", "w", newline="")) as csvfile_y:
         for i in range(0, len(list1)):
             all_data = list1[i][1:]
             writer = csv.writer(csvfile_y)
             writer.writerow(all_data)
-    
+
     print("\nscript_1 has finished working\n")
